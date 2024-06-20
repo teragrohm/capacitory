487fa6c0be81b7a1a2367f5f1fde65f37959e476
teragrohm <136333284+teragrohm@users.noreply.github.com>
2024-06-20 13:21:58 +0800

    Update script.js

--- objectDetection/script.js
+++ objectDetection/script.js
@@ -57,10 +57,16 @@ function enableCam(event) {
   });
 }
 
-// Placeholder function for next step.
-function predictWebcam() {
-}
+// Store the resulting model in the global scope of our app.
+var model = undefined;
 
-// Pretend model has loaded so we can try out the webcam code.
-var model = true;
-demosSection.classList.remove('invisible');
+// Before we can use COCO-SSD class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment 
+// to get everything needed to run.
+// Note: cocoSsd is an external object loaded from our index.html
+// script tag import so ignore any warning in Glitch.
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel;
+  // Show demo section now model is ready to use.
+  demosSection.classList.remove('invisible');
+});
