778b5f494e743f2b8d2dfc663295450447f8e26a
teragrohm <136333284+teragrohm@users.noreply.github.com>
2024-06-20 13:14:07 +0800

    Update script.js

--- objectDetection/script.js
+++ objectDetection/script.js
@@ -37,4 +37,22 @@ if (getUserMediaSupported()) {
 
 // Placeholder function for next step. Paste over this in the next step.
 function enableCam(event) {
+  // Only continue if the COCO-SSD has finished loading.
+  if (!model) {
+    return;
+  }
+  
+  // Hide the button once clicked.
+  event.target.classList.add('removed');  
+  
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
+    video: true
+  };
+
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadeddata', predictWebcam);
+  });
 }
