c40f31cc8db78779684b303622d34d60d42abb20
teragrohm <136333284+teragrohm@users.noreply.github.com>
2024-06-20 13:11:17 +0800

    Update script.js

--- objectDetection/script.js
+++ objectDetection/script.js
@@ -14,7 +14,27 @@
  * limitations under the License.
  * =============================================================================
  */
-/*
-const status = document.getElementById('status');
-status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
-*/
+
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+const demosSection = document.getElementById('demos');
+const enableWebcamButton = document.getElementById('webcamButton');
+
+// Check if webcam access is supported.
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
+}
+
+// If webcam supported, add event listener to button for when user
+// wants to activate it to call enableCam function which we will 
+// define in the next step.
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener('click', enableCam);
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
+}
+
+// Placeholder function for next step. Paste over this in the next step.
+function enableCam(event) {
+}
